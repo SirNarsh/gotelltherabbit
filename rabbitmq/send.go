4fabca9f6778afc43ef5fdf200fc01601e8b13ee
Nawwar Elnarsh <sirnarsh@gmail.com>
2019-07-07 22:49:30 +0200

    Implement HTTP2Rabbit publisher part

--- rabbitmq/send.go
+++ rabbitmq/send.go
@@ -1,13 +1,22 @@
 package rabbitmq
 
 import (
+	"fmt"
 	"log"
 
+	"github.com/sirnarsh/gotelltherabbit/readconf"
 	"github.com/streadway/amqp"
 )
 
-func Send() {
-	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+/*
+Send a message to exchange with body
+If exchange is not declared yet this will also declare the exchange
+*/
+func Send(exchangeName string, body []byte) {
+
+	var conf = readconf.GetGeneral()
+
+	conn, err := amqp.Dial(conf.RabbitMQServer)
 	failOnError(err, "Failed to connect to RabbitMQ")
 	defer conn.Close()
 
@@ -15,26 +24,22 @@ func Send() {
 	failOnError(err, "Failed to open a channel")
 	defer ch.Close()
 
-	q, err := ch.QueueDeclare(
-		"hello", // name
-		false,   // durable
-		false,   // delete when unused
-		false,   // exclusive
-		false,   // no-wait
-		nil,     // arguments
-	)
-	failOnError(err, "Failed to declare a queue")
+	// Declare Exchange to send message to, ideally exchange should be already declared and binded to queues
+	{
+		err := ch.ExchangeDeclare(exchangeName, "fanout", true, false, false, false, nil)
+		failOnError(err, fmt.Sprintf("Failed to declare exchange '%s'", exchangeName))
+		log.Printf("Declared or found exchange %s", exchangeName)
+	}
 
-	body := "Hello World!"
 	err = ch.Publish(
-		"",     // exchange
-		q.Name, // routing key
-		false,  // mandatory
-		false,  // immediate
+		exchangeName, // exchange
+		"",           // routing key
+		false,        // mandatory
+		false,        // immediate
 		amqp.Publishing{
 			ContentType: "text/plain",
-			Body:        []byte(body),
+			Body:        body,
 		})
-	log.Printf(" [x] Sent %s", body)
+	log.Printf(" [x] Sent msg to exchange %s", exchangeName)
 	failOnError(err, "Failed to publish a message")
 }
