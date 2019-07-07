ac0df04d7e1a3bdb5fbe5c4a63d72d41a53ec59f
Nawwar Elnarsh <sirnarsh@gmail.com>
2019-07-07 22:50:02 +0200

    Update main.go to load config and use listner

--- main.go
+++ main.go
@@ -1,13 +1,29 @@
 package main
 
 import (
-	"fmt"
+	"log"
 
 	"github.com/sirnarsh/gotelltherabbit/rabbitmq"
+	"github.com/sirnarsh/gotelltherabbit/readconf"
 )
 
 func main() {
-	fmt.Print("Testing")
-	rabbitmq.Send()
-	rabbitmq.Receive()
+	log.Println("Starting...")
+	readconf.CheckAllRequiredFiles()
+	var conf = readconf.GetGeneral()
+
+	if conf.EnableRabbit2HTTP {
+		log.Println("Starting RabbitMQ Listener")
+		rabbitmq.Receive()
+	} else {
+		log.Println("Rabbit 2 HTTP is not enabled in general.json")
+	}
+
+	if conf.EnableHTTP2Rabbit {
+		log.Println("Starting HTTP Listener")
+		log.Println("Warning, HTTP listener is not implemented yet")
+		// @todo add server listening to HTTP to and use rabbitmq.Send()
+	} else {
+		log.Println("HTTP to Rabbit is not enabled in general.json")
+	}
 }
