41f912bb82f979baf73b81bd48f1dab3c9dfcd18
Nawwar Elnarsh <sirnarsh@gmail.com>
2019-07-07 22:49:11 +0200

    Implement Rabbit2HTTP listener part

--- rabbitmq/receive.go
+++ rabbitmq/receive.go
@@ -1,15 +1,20 @@
 package rabbitmq
 
 import (
+	"fmt"
 	"log"
 
+	"github.com/sirnarsh/gotelltherabbit/readconf"
 	"github.com/streadway/amqp"
 )
 
-/* Receive messages from queue
- */
+// Receive messages from rabbit mq and send to http
 func Receive() {
-	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+
+	var conf = readconf.GetGeneral()
+	var r2hConf = readconf.GetR2H()
+
+	conn, err := amqp.Dial(conf.RabbitMQServer)
 	failOnError(err, "Failed to connect to RabbitMQ")
 	defer conn.Close()
 
@@ -17,35 +22,61 @@ func Receive() {
 	failOnError(err, "Failed to open a channel")
 	defer ch.Close()
 
-	q, err := ch.QueueDeclare(
-		"hello", // name
-		false,   // durable
-		false,   // delete when unused
-		false,   // exclusive
-		false,   // no-wait
-		nil,     // arguments
-	)
-	failOnError(err, "Failed to declare a queue")
+	// Declare our listener queue
+	{
+		_, err := ch.QueueDeclare(
+			r2hConf.ListenQueue, // name
+			true,                // durable
+			false,               // delete when unused
+			false,               // exclusive
+			false,               // no-wait
+			nil,                 // arguments
+		)
+		failOnError(err, "Failed to declare a queue")
+	}
+
+	// Bind the queue we are consuming with all exchanges we want to listen to.
+	for _, exchange := range r2hConf.BindExchanges {
+		{
+			err := ch.ExchangeDeclare(exchange.ExchangeName, "fanout", true, false, false, false, nil)
+			failOnError(err, fmt.Sprintf("Failed to bind Exchange '%s' to Queue '%s'", r2hConf.ListenQueue, exchange.ExchangeName))
+			log.Printf("Declared or found exchange %s", exchange.ExchangeName)
+		}
+		{
+			err := ch.QueueBind(
+				r2hConf.ListenQueue,
+				"",
+				exchange.ExchangeName,
+				false,
+				nil,
+			)
+			failOnError(err, fmt.Sprintf("Failed to bind Exchange '%s' to Queue '%s'", r2hConf.ListenQueue, exchange.ExchangeName))
+			log.Printf("Bind Exchange '%s' to listener queue '%s'", exchange.ExchangeName, r2hConf.ListenQueue)
+		}
+	}
 
 	msgs, err := ch.Consume(
-		q.Name, // queue
-		"",     // consumer
-		true,   // auto-ack
-		false,  // exclusive
-		false,  // no-local
-		false,  // no-wait
-		nil,    // args
+		r2hConf.ListenQueue, // queue
+		"",                  // consumer
+		false,               // auto-ack
+		false,               // exclusive
+		false,               // no-local
+		false,               // no-wait
+		nil,                 // args
 	)
 	failOnError(err, "Failed to register a consumer")
 
-	forever := make(chan bool)
+	listener := make(chan bool)
 
 	go func() {
 		for d := range msgs {
-			log.Printf("Received a message: %s", d.Body)
+			log.Printf("Received a message: %s, from exchange %s with routing %s", d.Body, d.Exchange, d.RoutingKey)
+			// @todo ACK Should be send after we send the HTTP request and get code 200
+			// For now we ACK message after consuming directly
+			d.Ack(false)
 		}
 	}()
 
 	log.Printf(" [*] Waiting for messages. To exit press CTRL+C")
-	<-forever
+	<-listener
 }
