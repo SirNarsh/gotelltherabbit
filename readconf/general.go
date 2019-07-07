df227a3f0fc9dc89fac89be07ecd6866cd89e03d
Nawwar Elnarsh <sirnarsh@gmail.com>
2019-07-07 23:42:29 +0200

    Add ServerBind paramter to general config

--- readconf/general.go
+++ readconf/general.go
@@ -2,6 +2,6 @@ package readconf
 
 // GeneralConf used to deserialize general.json Contains enabled features & RabbitMQ Server
 type GeneralConf struct {
-	RabbitMQServer                       string
+	RabbitMQServer, ServerBind           string
 	EnableRabbit2HTTP, EnableHTTP2Rabbit bool
 }
